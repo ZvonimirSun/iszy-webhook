d79da3786dfb4f5058a10ab0cc617f9db27d6c00
ZvonimirSun <hi@iszy.xyz>
2022-05-05 14:04:38 +0800

    feat: support target branch

--- index.js
+++ index.js
@@ -11,19 +11,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    if (cmd.length) {
-        const child = spawn(cmd[0], cmd.slice(1));
-        let resp = "";
-        child.stdout.on("data", (buffer) => {
-            resp += buffer.toString();
-        });
-        child.stdout.on("end", () => {
-            console.log(resp);
-            res.send(resp);
-        });
+    if (!req.body) {
+        return res.sendStatus(400);
     } else {
-        console.log("No command specified");
-        res.send("No command specified");
+        req.body.ref = req.body.ref || "";
+    }
+    let hasTarget = false;
+    if (process.env.TARGET_BRANCH) {
+        const branches = process.env.TARGET_BRANCH.split(",");
+        for (let i = 0; i < branches.length; i++) {
+            if (req.body.ref.slice(11).indexOf(branches[i]) > -1) {
+                hasTarget = true;
+                break;
+            }
+        }
+    } else {
+        hasTarget = true;
+    }
+    if (hasTarget) {
+        if (cmd.length) {
+            const child = spawn(cmd[0], cmd.slice(1));
+            let resp = "";
+            child.stdout.on("data", (buffer) => {
+                resp += buffer.toString();
+            });
+            child.stdout.on("end", () => {
+                console.log(resp);
+                res.send(resp);
+            });
+        } else {
+            console.log("No command specified");
+            res.send("No command specified");
+        }
+    } else {
+        res.send("Nothing to do");
     }
 });
 
